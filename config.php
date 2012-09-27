28d3097492ca83cbaac6bee90dd76b0c905f5b0c
Marc Bernet <mbernet@gmail.com>
2012-09-27 11:02:06 +0200

    config singleton class

---
+++ config.php
@@ -0,0 +1,40 @@
+<?php
+class Config
+{
+   
+    private static $instance;
+    private $config = array();
+    private function __construct() 
+    {    
+    	
+    }
+    
+    public function read($key)
+    {
+    	if(isset($this->config[$key]))
+    	{
+    		return $this->config[$key];
+    	}
+    	return false;
+    }
+    
+    public function write($key, $value)
+    {
+    	$this->config[$key] = $value;
+    }
+    public static function get()
+    {
+        if (!isset(self::$instance)) {
+            $c = __CLASS__;
+            self::$instance = new $c;
+        }
+
+        return self::$instance;
+    }
+    
+    public function __clone()
+    {
+        trigger_error('Clone is not allowed.', E_USER_ERROR);
+    }
+
+}
