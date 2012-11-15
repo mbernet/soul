2afd578b6329aba37defab264c5f45f2f0272d48
Marc Bernet <mbernet@gmail.com>
2012-11-15 11:21:21 +0100

    autoversion file

--- functions.php
+++ functions.php
@@ -7,3 +7,28 @@ function __($text)
 	else
 		return gettext($text);
 }
+
+function debug($obj)
+{
+	if(DEBUG)
+	{
+	    echo "<pre>";
+	    print_r($obj);
+	    echo "</pre>";
+	}
+    
+}
+
+function auto_version($file)
+{
+	if(strpos($file, '/') !== 0 || !file_exists($_SERVER['DOCUMENT_ROOT'] . $file))
+	return $file;
+	
+	$asset = CURRENT_ASSET;
+	return preg_replace('{\\.([^./]+)$}', ".$asset.\$1", $file);
+}
+
+function version_file($file)
+{
+	return CURRENT_ASSET;
+}
--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-$time_start = microtime(true);
 require('core/config.php');
-require('core/functions.php');
 require('app/config/config.php');
+require('core/functions.php');
 require('core/request.php');
 require('core/front.php');
 require('core/controller.php');
@@ -18,10 +17,6 @@ set_exception_handler(array('SoulException', 'catchException'));
 
 $array_uri = Router::get_route();
 FrontController::dispatch($array_uri['controller'],$array_uri['function'], $array_uri['file'], $_GET, $_POST, $array_uri['vars'], $array_uri['args']); 
-$time_end = microtime(true);
-$time = $time_end - $time_start;
-
-//echo "<!-- $time seconds -->";
        
 function __autoload($name) {
     Autoloader::autoLoadFile($name);
