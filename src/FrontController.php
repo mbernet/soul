a6aca348bcacbe2805221aec977b3e4dd9c16090
Marc Bernet <mbernet@gmail.com>
2016-11-02 11:35:53 +0100

    404

--- src/FrontController.php
+++ src/FrontController.php
@@ -3,23 +3,28 @@ namespace SoulFramework;
 class FrontController
 {
 	static function dispatch($controller, $action, $vars_get, $vars_post, $vars_uri, $vars_arg)
-	{              
-		$controller_inst = new $controller();
-        if(method_exists($controller_inst,$action) || method_exists($controller_inst, '__call'))
-        {
-                $controller_inst->name = $controller;
-                $controller_inst->action = $action;
-                $controller_inst->setRequest($vars_get, $vars_post, $vars_uri, $vars_arg);
-                $controller_inst->beforeAction();
-                $avoid_call_action = false;
-				if(!$avoid_call_action) {
-                    $controller_inst->$action();
-				}
-                $controller_inst->afterAction();
+    {
+        if(class_exists($controller)) {
+                $controller_inst = new $controller();
+                if(method_exists($controller_inst,$action) || method_exists($controller_inst, '__call'))
+                {
+                        $controller_inst->name = $controller;
+                        $controller_inst->action = $action;
+                        $controller_inst->setRequest($vars_get, $vars_post, $vars_uri, $vars_arg);
+                        $controller_inst->beforeAction();
+                        $avoid_call_action = false;
+                        if(!$avoid_call_action) {
+                                $controller_inst->$action();
+                        }
+                        $controller_inst->afterAction();
+                }
+                else {
+                        throw new \Exception("Missing method $action in $controller", 404);
+                }
         }
         else
         {
-                throw new \Exception("Missing method $action in $controller", 404);
+                throw new \Exception("Controller $controller does not exists", 404);
         }
 	}
 }
