1d1bde7f5f380b575b947b357be03dce024fe896
Marc Bernet <mbernet@gmail.com>
2017-02-28 13:31:33 +0100

    old fashioned way to log

--- object.php
+++ object.php
@@ -3,7 +3,30 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 class Object
 {
-	protected function log($msg, $file = null, $type = null)
+
+	protected function log($msg, $file = null)
+	{
+		if($file == null)
+		{
+			$file = 'general';
+		}
+
+		$log = LOG_DIR . DS . $file . '.log';
+		if(is_array($msg) || is_object($msg))
+		{
+			$msg = print_r($msg, true);
+		}
+
+		$entry = date("Y-m-d H:i:s")." # ".$msg."\r\n";
+		$fh = fopen($log, 'a');
+		if($fh)
+		{
+			fwrite($fh, $entry);
+			fclose($fh);
+		}
+	}
+
+	protected function monolog($msg, $file = null, $type = null)
 	{
 		$context = [];
 		if(is_array($msg) || is_object($msg))
