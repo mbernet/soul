4a8283d4f38d3a62bc8db06b2c1b4117eae9bfe1
Marc Bernet <mbernet@gmail.com>
2017-11-06 09:30:18 +0100

    allowed headers

--- src/Soul.php
+++ src/Soul.php
@@ -23,7 +23,13 @@ class Soul {
         if($httpMethod == 'OPTIONS') {
             header('Access-Control-Allow-Origin: *');
             header('Access-Control-Allow-Methods: GET, POST, DELETE, PUT, PATCH, OPTIONS');
-            header('Access-Control-Allow-Headers: Content-Type, ApiKey, Authorization');
+            if(defined('ALLOWED_HEADERS')) {
+                header('Access-Control-Allow-Headers: '.ALLOWED_HEADERS);
+            }
+            else {
+                header('Access-Control-Allow-Headers: Content-Type, ApiKey, Authorization');
+            }
+
             header($_SERVER['SERVER_PROTOCOL'] . ' 200 OK', true, 200);
             exit();
         }
